b73435ebbe3331d073f2b3adba7a637fd854f4ea
Jose Enrique Rios Anaya <joseenriqueriosanaya@gmail.com>
2023-11-01 19:04:26 -0500

    1 NOV

---
+++ Servicios/RepositorioCuentas.cs
@@ -0,0 +1,30 @@
+﻿using Dapper;
+using ManejoPresupuestos.Models;
+using Microsoft.Data.SqlClient;
+
+namespace ManejoPresupuestos.Servicios
+{
+
+    public interface IRepositorioCuentas
+    {
+        Task Crear(Cuenta cuenta);
+    }
+    public class RepositorioCuentas: IRepositorioCuentas
+    {
+        private readonly string connectionString;
+
+        public RepositorioCuentas(IConfiguration configuration) 
+        {
+            connectionString = configuration.GetConnectionString("DefaultConnection");
+        }
+
+        public async Task Crear(Cuenta cuenta)
+        {
+            using var connection = new SqlConnection(connectionString);
+            var id = await connection.QuerySingleAsync<int>("INSERT INTO Cuentas (Nombre, TipoCuentaId, Descripcion, Balance) VALUES (@Nombre, @TipoCuentaId, @Descipcion, @Balance); " +
+                "SELECT SCOPE_IDENTTITY();", cuenta);
+
+            cuenta.Id = id;
+        }
+    }
+}
